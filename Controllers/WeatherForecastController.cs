dcf85b4f57b2d933bfd7a94c439d253b88f19d02
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-09-09 13:47:42 +0200

    test triger

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace TestAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summari = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
